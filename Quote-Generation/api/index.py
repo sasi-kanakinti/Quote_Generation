4531353d22d1a72b8c0e0273cc49155bc28224ad
Sasidhar <9908135565ks@gmail.com>
2025-12-05 00:40:56 +0530

    Backend modified

--- Quote-Generation/api/index.py
+++ Quote-Generation/api/index.py
@@ -1,29 +1,24 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-# from mangum import Mangum
+from mangum import Mangum
+
 from app.routers import auth, random_quote, favorites
 
 app = FastAPI()
-# handler = Mangum(app)
-
-origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+handler = Mangum(app)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Routers
 app.include_router(auth.router)
 app.include_router(random_quote.router)
 app.include_router(favorites.router)
 
 @app.get("/")
 def root():
-    return {"status": "backend running"}
+    return {"status": "backend running on vercel"}
